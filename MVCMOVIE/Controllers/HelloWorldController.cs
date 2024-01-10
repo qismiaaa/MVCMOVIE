72cf8b2f4d95f99c3e8550719f9d998ea649e988
qismi <qismi@QISMIA>
2024-01-10 10:47:55 +0800

    Add project files.

---
+++ MVCMOVIE/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVCMOVIE.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        //GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            return View();
+
+           // return "This is my default action...";
+        }
+
+        //
+        //GET" /HelloWorld/Welcome/
+        //Requires using System.Text.Encodings.Web;
+
+        public IActionResult Welcome(string name,int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello" + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+           // return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+            //return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is {numTimes}");
+           // return "This is the Welcome action method...";
+        }
+    }
+}
---
+++ MVCMOVIE/Data/MVCMOVIEContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCMOVIE.Models;
+
+namespace MVCMOVIE.Data
+{
+    public class MVCMOVIEContext : DbContext
+    {
+        public MVCMOVIEContext (DbContextOptions<MVCMOVIEContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCMOVIE.Models.Movie> Movie { get; set; } 
+    }
+}
